aacd26528be5b7fa3fadd4f527d94dbcadf2619d
krupa-rn <92620053+krupa-rn@users.noreply.github.com>
2021-11-09 12:55:03 +0530

    n

--- Instagram/Main.js
+++ Instagram/Main.js
@@ -5,8 +5,9 @@ import Icon from 'react-native-vector-icons/Ionicons'
 import Login from './Login';
 import userWelcome from './userWelcome';
 import userProfile from './userProfile';
-import { StatusBar } from 'react-native';
+import { StatusBar, Image } from 'react-native';
 import C from '../Instagram/constants';
+import logo from '../misc/'
 const Main =() => {
 
     //const LoggedIn = false;    //to get from Async? 
@@ -18,7 +19,8 @@ const Main =() => {
         <stack.Navigator initialRouteName='Login'>
         <stack.Screen name="Login" component={Login} options={{headerShown:false, header:null}}/>
         <stack.Screen name="userWelcome" component={userWelcome} />
-        <stack.Screen name="Profile" component={userProfile} options={{title: 'Instagram', 
+        <stack.Screen name="Profile" component={userProfile} options={{
+            headerTitle: ()=>{ <Icon name='md-camera-outline' size={30} color={C.insta_grey}/> }, 
             headerLeft: ()=> <Icon name='md-camera-outline' size={30} color={C.insta_grey}/>, 
             headerRight: ()=> <Icon name='md-paper-plane-outline' size={30} color={C.insta_grey}/>     }}>
         </stack.Screen>
